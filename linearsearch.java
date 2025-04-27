0abb5ce0f5ee76dfd128b4725d9b88d20adf9a85
Ashish-071 <nainiashishkumar@gmail.com>
2025-04-27 09:18:17 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
-
 public class LinearSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Input array size
+        
         System.out.print("Enter the number of elements in the array: ");
         int n = scanner.nextInt();
 
         int[] arr = new int[n];
 
-        // Input array elements
         System.out.println("Enter the elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        // Input the element to search
         System.out.print("Enter the element to search for: ");
         int target = scanner.nextInt();
 
-        // Perform linear search
         boolean found = false;
         for (int i = 0; i < n; i++) {
             if (arr[i] == target) {
